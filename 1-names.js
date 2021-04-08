9cae3dcf50fb031972c6a11b292f90871f23a182
Catherine <yliu7@hotmail.com>
2021-04-08 09:33:01 -0700

    first commit

---
+++ 1-names.js
@@ -0,0 +1,16 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const john = 'john'
+const peter = 'peter'
+
+const items = ['item1', 'item2']
+
+const person = {
+    name: 'bob',
+    email: 'bob@yahoo.com'
+}
+
+module.exports = { john, peter, items, person }
+
+console.log(module);
---
+++ app.js
@@ -0,0 +1,5 @@
+const { john, peter } = require('./1-names');
+const sayHi = require('./1-utils');
+
+sayHi(john)       // Hello there john
+sayHi(peter)      // Hello there peter
